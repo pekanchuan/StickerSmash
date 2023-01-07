c0e4cdce7131f37ea86271de3102ff91d913531e
CHia <dozeuto@gmail.com>
2023-01-07 22:27:10 +0800

    dividing components into files

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native-web";
+import { StyleSheet, View } from "react-native";
+
+import ImageViewer from "./components/ImageViewer";
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 const App = () => {
   return (
     <View style={styles.container}>
-      {/* <Text style={{ color: "#fff" }}>
-        Open up App.js to start working on your app!
-      </Text> */}
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -29,10 +28,5 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
     paddingTop: 58,
-  },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
   },
 });
