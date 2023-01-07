d0f84863fbd1cd643d89f3fe0608065c8d7990de
CHia <dozeuto@gmail.com>
2023-01-07 21:59:48 +0800

    display the image

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native-web";
+import { StyleSheet, Text, View, Image } from "react-native-web";
+
+const PlaceholderImage = require("./assets/images/background-image.png");
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={{ color: "#fff" }}>
+      {/* <Text style={{ color: "#fff" }}>
         Open up App.js to start working on your app!
-      </Text>
+      </Text> */}
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,5 +25,14 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#25292e",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });
