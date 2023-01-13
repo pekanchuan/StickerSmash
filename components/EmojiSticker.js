40385658062dc1b8d975e8e08cdfd298917986dd
CHia <dozeuto@gmail.com>
2023-01-13 10:22:22 +0800

    create animated components

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,9 +1,13 @@
 import { View, Image } from "react-native";
 
+import Animated from "react-native-reanimated";
+
+const AnimatedImage = Animated.createAnimatedComponent(Image);
+
 const EmojiSticker = ({ imageSize, stickerSource }) => {
   return (
     <View style={{ top: -350 }}>
-      <Image
+      <AnimatedImage
         source={stickerSource}
         resizeMode="contain"
         style={{ width: imageSize, height: imageSize }}
